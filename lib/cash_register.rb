a384ec074f039bd2146af7637accec970f6f98c8
Alexandra Iglesia <alexxandraiglesia@gmail.com>
2022-11-02 13:34:19 -0400

    commit tree dfb22b7075c0eda32b8c362b4a65ef53f6f81547

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,38 @@
+require 'pry'
 
+class CashRegister
+
+    attr_accessor :discount, :total, :items, :last_transaction
+
+    def initialize discount = 0
+        @discount = discount
+        @total = 0
+        @items = []
+        @last_transaction = 0
+    end
+
+    def add_item title, price, quantity = 1
+        self.total += price * quantity
+        quantity.times do
+            self.items << title
+        end
+        self.last_transaction = price * quantity
+    end
+
+    def apply_discount
+        if self.discount == 0
+            "There is no discount to apply."
+        else
+            # turn the current discount into a decimal
+            percent = self.discount.to_f / 100 # .to_f turns integer into a float(decimal)
+            self.total = self.total - (self.total * percent)
+            "After the discount, the total comes to $#{self.total.to_i}."
+        end
+    end
+
+    def void_last_transaction
+        self.total -= self.last_transaction
+        self.last_transaction = 0
+    end
+
+end
